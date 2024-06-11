36d965e3c49996f98bcca3522c078575779edf9f
Pranay Heerekar <heerapranaykumar@gmail.com>
2024-06-11 16:44:05 +0530

    added logintest feature

--- src/main/java/gitHubDemo/BasicJAVAClass.java
+++ src/main/java/gitHubDemo/BasicJAVAClass.java
@@ -3,7 +3,8 @@ package gitHubDemo;
 public class BasicJAVAClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
 
 	}
 
